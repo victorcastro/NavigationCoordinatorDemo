8725bd56c82452d25783e02a68027d21c62c11fa
Alex Nagy <rebeloperalpar@gmail.com>
2024-02-18 09:21:44 +0200

    Initial Commit

---
+++ NavigationCoordinatorDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationCoordinatorDemo
+//
+//  Created by Alex Nagy on 18.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationCoordinatorDemo/NavigationCoordinatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationCoordinatorDemoApp.swift
+//  NavigationCoordinatorDemo
+//
+//  Created by Alex Nagy on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationCoordinatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
